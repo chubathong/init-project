6870f835fac5094034f1df50a2b45bc6266966b7
phat <chubathongp2000@gmail.com>
2024-02-19 17:20:53 +0700

    varialbe

--- src/project1_helloworld/HelloWorld.java
+++ src/project1_helloworld/HelloWorld.java
@@ -2,8 +2,7 @@ package project1_helloworld;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello world test");
-		System.out.println("Hello world test");
-		System.out.println("Hello world test");
+		int test = 5 + 9 * 3 - 10 / 2;
+		System.out.println("check test: " + test);
 	}
 }
