08a4eb4cef3792f632978dc63c3a812c8d6397f4
phat <chubathongp2000@gmail.com>
2024-02-19 16:40:41 +0700

    first commit

---
+++ src/project1_helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+package project1_helloworld;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world test");
+		System.out.println("Hello world test");
+		System.out.println("Hello world test");
+	}
+}
